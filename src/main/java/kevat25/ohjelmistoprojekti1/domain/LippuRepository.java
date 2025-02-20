b5a6601c93b520a4b2bcb5bbb95dc355f63a39be
Samuli Tani <samuli.tani@gmail.com>
2025-02-20 12:35:49 +0200

    @Repository annotataatio vaihdettu muotoon @RepositoryRestResource(collectionResourceRel = '{entityn nimi monikossa}', path = '{entityn nimi monikossa}'), eli REST API käyttöliittymän hakemistot ja JSON-avaimet _links-tiedoissa vastaavat nyt tietokantojen taulujen nimiä

--- src/main/java/kevat25/ohjelmistoprojekti1/domain/LippuRepository.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/domain/LippuRepository.java
@@ -1,9 +1,10 @@
 package kevat25.ohjelmistoprojekti1.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "liput", path = "liput")
 public interface LippuRepository extends CrudRepository<Lippu, Long> {
 
 }
--- src/main/java/kevat25/ohjelmistoprojekti1/domain/TyontekijaRepository.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/domain/TyontekijaRepository.java
@@ -1,9 +1,10 @@
 package kevat25.ohjelmistoprojekti1.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "tyontekijat", path = "tyontekijat")
 public interface TyontekijaRepository extends CrudRepository<Tyontekija, Long> {
 
 }
