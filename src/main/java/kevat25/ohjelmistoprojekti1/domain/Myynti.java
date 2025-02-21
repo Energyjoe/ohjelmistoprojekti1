05926679f61c8297ef87142145949c8c599c323a
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-02-21 23:29:37 +0200

    OneToMany suhde tehty myynti -luokasta lippu -luokaan. Inline-kommentit lisätty molempiin luokkiin

--- src/main/java/kevat25/ohjelmistoprojekti1/domain/Myynti.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/domain/Myynti.java
@@ -1,8 +1,9 @@
 package kevat25.ohjelmistoprojekti1.domain;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,34 +11,39 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "myynnit")
+@Table(name = "myynnit") // Määrittää taulun, johon Myynti-luokan instanssit tallennetaan
 public class Myynti {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "myynti_id")
+    @GeneratedValue(strategy = GenerationType.AUTO) // Myynti-id generoidaan automaattisesti tietokannan avulla
+    @Column(name = "myynti_id") // Taulun sarake, jossa säilytetään myynti-id:tä
     private Long myyntiId;
 
-    @ManyToOne
-    @JoinColumn(name = "tyontekija_id")
-    private Tyontekija tyontekijat;
+    @ManyToOne // Määrittää, että tämä on monen myynnin ja yhden työntekijän välinen suhde
+    @JoinColumn(name = "tyontekija_id") // Liittää työntekijän myyntiin työntekijä-id:n kautta
+    private Tyontekija tyontekija;
 
     private LocalDateTime myyntiaika;
     private String email;
 
+    // Liittää liput myyntiin; kaikki liput poistetaan, kun myynti poistetaan
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "myynti")
+    private List<Lippu> liput;
 
     public Myynti() {
     }
 
-    public Myynti(LocalDateTime myyntiaika, String email, Tyontekija tyontekijat) {
+    public Myynti(LocalDateTime myyntiaika, String email, Tyontekija tyontekija) {
         this.myyntiaika = myyntiaika;
         this.email = email;
-        this.tyontekijat = tyontekijat;
+        this.tyontekija = tyontekija;
     }
 
+    //Getterit ja setterit myyntitapahtuman tiedoille
     public Long getMyyntiId() {
         return myyntiId;
     }
@@ -62,19 +68,30 @@ public class Myynti {
         this.email = email;
     }
 
-    public Tyontekija getTyontekijat() {
-        return tyontekijat;
+    public Tyontekija getTyontekija() {
+        return tyontekija;
     }
 
-    public void setTyontekijat(Tyontekija tyontekijat) {
-        this.tyontekijat = tyontekijat;
+    public void setTyontekija(Tyontekija tyontekija) {
+        this.tyontekija = tyontekija;
     }
 
+    public List<Lippu> getLiput() {
+        return liput;
+    }
+
+    public void setLiput(List<Lippu> liput) {
+        this.liput = liput;
+    }
+
+    /* Ehdollinen toString tulostaa kaikki tiedot jos työntekijä on määritelty,
+     * muissa tapauksissa pelkästään myyntitapahtuman perustiedot
+     */
     @Override
     public String toString() {
-        if (this.tyontekijat != null) {
-        return "Myynnit [myyntiId=" + myyntiId + ", myyntiaika=" + myyntiaika + ", email=" + email + ", tyontekijat="
-                + this.getTyontekijat() + "]";
+        if (this.tyontekija != null) {
+        return "Myynnit [myyntiId=" + myyntiId + ", myyntiaika=" + myyntiaika + ", email=" + email + ", tyontekija="
+                + this.getTyontekija() + "]";
         } else {
             return "Myynnit [myyntiId=" + myyntiId + ", myyntiaika=" + myyntiaika + ", email=" + email + "]";
         }
