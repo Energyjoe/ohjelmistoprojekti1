fec29a9e65852cc64f906790e483458c889170c7
Samuli Tani <samuli.tani@gmail.com>
2025-02-27 15:06:38 +0200

    Postinumero.java -> @OneToMany yhteys Tapahtumapaikka.java ja Tyontekija.java

--- src/main/java/kevat25/ohjelmistoprojekti1/domain/Postinumero.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/domain/Postinumero.java
@@ -6,7 +6,10 @@ import jakarta.persistence.Entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
+import jakarta.persistence.CascadeType;
 
 @Entity
 @Table(name = "postinumerot")
@@ -15,6 +18,12 @@ public class Postinumero {
     private String postinumero;
 
     private String paikkakunta;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "postinumero")
+    private List<Tapahtumapaikka> tapahtumapaikat;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "postinumero")
+    private List<Tyontekija> tyontekijat;
 
     public Postinumero() {
     }
