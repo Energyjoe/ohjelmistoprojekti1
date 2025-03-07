0955d3c11e210bac610d264a4eb0c662f460e941
lottagardin <lotta.gardin@gmail.com>
2025-03-07 16:30:30 +0200

    LippuDTO ja MyyntiDTO valmiit

--- src/main/java/kevat25/ohjelmistoprojekti1/domain/LippuDTO.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/domain/LippuDTO.java
@@ -1,49 +1,109 @@
 package kevat25.ohjelmistoprojekti1.domain;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class LippuDTO {
 
     private Long lippuId;
     private String tarkistuskoodi;
-    private Long tapahtumalippuId; // Viite tapahtumalippu-id:hen
     private Long myyntiId; // Viite myynti-id:hen
+    private Long tapahtumalippuId; // Viite tapahtumalippu-id:hen
 
-    // Getterit ja setterit
+    //Tapahtuma-entitystä
+    private String tapahtumanNimi; 
+    private LocalDateTime alkuaika;
 
-    public Long getLippuId() {
-        return lippuId;
+    //Tapahtumalippu-entitystä
+    private BigDecimal hinta;
+    private Asiakastyyppi asiakastyyppi;
+
+
+   
+    
+    // Parametriton konstruktori 
+    public LippuDTO() {
     }
-
-    public void setLippuId(Long lippuId) {
+    
+    // Parametrillinen konstruktori
+    public LippuDTO(Long lippuId, String tarkistuskoodi, Long myyntiId, Long tapahtumalippuId, 
+                     String tapahtumanNimi, LocalDateTime alkuaika, BigDecimal hinta, 
+                    Asiakastyyppi asiakastyyppi) {
         this.lippuId = lippuId;
-    }
-
-    public String getTarkistuskoodi() {
-        return tarkistuskoodi;
-    }
-
-    public void setTarkistuskoodi(String tarkistuskoodi) {
         this.tarkistuskoodi = tarkistuskoodi;
-    }
-
-    public Long getTapahtumalippuId() {
-        return tapahtumalippuId;
-    }
-
-    public void setTapahtumalippuId(Long tapahtumalippuId) {
-        this.tapahtumalippuId = tapahtumalippuId;
-    }
-
-    public Long getMyyntiId() {
-        return myyntiId;
-    }
-
-    public void setMyyntiId(Long myyntiId) {
         this.myyntiId = myyntiId;
+        this.tapahtumalippuId = tapahtumalippuId;
+        this.tapahtumanNimi = tapahtumanNimi;
+        this.alkuaika = alkuaika;
+        this.hinta = hinta;
+        this.asiakastyyppi = asiakastyyppi;
+        }
+    
+        // Getterit ja setterit
+    
+        public Long getLippuId() {
+            return lippuId;
+        }
+    
+        public void setLippuId(Long lippuId) {
+            this.lippuId = lippuId;
+        }
+    
+        public String getTarkistuskoodi() {
+            return tarkistuskoodi;
+        }
+    
+        public void setTarkistuskoodi(String tarkistuskoodi) {
+            this.tarkistuskoodi = tarkistuskoodi;
+        }
+    
+        public Long getMyyntiId() {
+            return myyntiId;
+        }
+    
+        public void setMyyntiId(Long myyntiId) {
+            this.myyntiId = myyntiId;
+        }
+    
+        public Long getTapahtumalippuId() {
+            return tapahtumalippuId;
+        }
+    
+        public void setTapahtumalippuId(Long tapahtumalippuId) {
+            this.tapahtumalippuId = tapahtumalippuId;
+        }
+    
+        public String getTapahtumanNimi() {
+            return tapahtumanNimi;
+        }
+    
+        public void setTapahtumanNimi(String tapahtumanNimi) {
+            this.tapahtumanNimi = tapahtumanNimi;
+        }
+    
+        public LocalDateTime getAlkuaika() {
+            return alkuaika;
+        }
+    
+        public void setAlkuaika(LocalDateTime alkuaika) {
+            this.alkuaika = alkuaika;
+        }
+    
+        public BigDecimal getHinta() {
+            return hinta;
+        }
+    
+        public void setHinta(BigDecimal hinta) {
+            this.hinta = hinta;
+        }
+    
+        public Asiakastyyppi getAsiakastyyppi() {
+            return asiakastyyppi;
+        }
+    
+        public void setAsiakastyyppi(Asiakastyyppi asiakastyyppi) {
+            this.asiakastyyppi = asiakastyyppi;
+        }
     }
+    
 
-    @Override
-    public String toString() {
-        return "LippuDTO [lippuId=" + lippuId + ", tarkistuskoodi=" + tarkistuskoodi + ", tapahtumalippuId="
-                + tapahtumalippuId + ", myyntiId=" + myyntiId + "]";
-    }
-}
--- src/main/java/kevat25/ohjelmistoprojekti1/domain/MyyntiDTO.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/domain/MyyntiDTO.java
@@ -1,30 +1,74 @@
 package kevat25.ohjelmistoprojekti1.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MyyntiDTO {
-    private String email;
-    private Long tyontekijaId;
 
-    public MyyntiDTO() {
-    }
+//Myynti-entitystä
+private LocalDateTime myyntiaika;
+private Tyontekija tyontekija;
+private String email;
+private Long myyntiId;
 
-    public MyyntiDTO(String email, Long tyontekijaId) {
-        this.email = email;
-        this.tyontekijaId = tyontekijaId;
-    }
+//LippuDTO:t lisätään tänne listana, jolloin saadaan jokaisen lipun yksittäiset tiedot haettua tätä kautta
+private List<LippuDTO> liput;
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//Getterit
 
-    public Long getTyontekijaId() {
-        return tyontekijaId;
-    }
-
-    public void setTyontekijaId(Long tyontekijaId) {
-        this.tyontekijaId = tyontekijaId;
-    }
+public LocalDateTime getMyyntiaika() {
+    return myyntiaika;
 }
+
+public Tyontekija getTyontekija() {
+    return tyontekija;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public Long getMyyntiId() {
+    return myyntiId;
+}
+
+//Setterit
+
+
+
+public void setMyyntiaika (LocalDateTime myyntiaika) {
+    this.myyntiaika = myyntiaika;
+}
+
+public void setTyontekija (Tyontekija tyontekija) {
+    this.tyontekija = tyontekija;
+}
+
+public void setEmail (String email) {
+    this.email = email;
+}
+
+public void setMyyntiId(Long myyntiId) {
+    this.myyntiId = myyntiId;
+}
+
+//Parametriton konstruktori:
+public  MyyntiDTO() {
+
+}
+
+//Konstruktori: 
+public MyyntiDTO(LocalDateTime myyntiaika, Tyontekija tyontekija, 
+String email, Long myyntiId, List<LippuDTO> liput) {
+this.myyntiaika = myyntiaika;
+this.tyontekija = tyontekija;
+this.email = email;
+this.myyntiId = myyntiId;
+this.liput = liput;
+}
+
+
+}
+
+
