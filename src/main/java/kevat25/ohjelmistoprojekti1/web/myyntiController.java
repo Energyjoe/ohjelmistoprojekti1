55dd6dcdf53efe4cdc884faa452e9d9c018e08cb
Lehtimaeki <anni.tuulevi.lehtimaki@gmail.com>
2025-03-09 20:18:38 +0200

    Delete tehty

--- src/main/java/kevat25/ohjelmistoprojekti1/web/myyntiController.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/web/myyntiController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,6 +70,49 @@ public class myyntiController {
         lippuRepository.save(lippu);
 
         return ResponseEntity.ok(myynti);
+    }
+
+    // Muokkaa myyntitapahtumaa
+    @PatchMapping("/{myyntiId}")
+    public ResponseEntity<?> muokkaaMyyntia(@PathVariable Long myyntiId, @RequestBody MyyntiDTO myyntiDTO) {
+        Optional<Myynti> myyntiOpt = myyntiRepository.findById(myyntiId);
+        if (myyntiOpt.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Myyntitapahtumaa ei löytynyt");
+        }
+
+        Myynti myynti = myyntiOpt.get();
+
+        if (!isValidEmail(myyntiDTO.getEmail())) {
+            return ResponseEntity.badRequest().body("Virheellinen sähköpostiosoite.");
+        } else {
+            myynti.setEmail(myyntiDTO.getEmail());
+        }
+
+        if (myyntiDTO.getTyontekijaId() != null) {
+            Optional<Tyontekija> tyontekijaOpt = tyontekijaRepository.findById(myyntiDTO.getTyontekijaId());
+            if (tyontekijaOpt.isEmpty()) {
+                return ResponseEntity.badRequest().body("Työntekijää ei löytynyt.");
+            }
+            myynti.setTyontekija(tyontekijaOpt.get());
+        }
+
+        myyntiRepository.save(myynti);
+        return ResponseEntity.ok(myynti);
+    }
+
+    //Muokkaa myyntitapahtuman lippuja (työn alla)
+
+    //Poista myyntitapahtuma
+    @DeleteMapping("/{myyntiId}")
+    public ResponseEntity<Void> poistaMyynti(@PathVariable Long myyntiId) {
+        Optional<Myynti> myynti = myyntiRepository.findById(myyntiId);
+
+        if (myynti.isPresent()) {
+            myyntiRepository.deleteById(myyntiId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
 }
