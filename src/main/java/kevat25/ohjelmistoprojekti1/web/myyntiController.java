bd29f91d9397aeda24f3e5ff55a1925726988256
JenniEmilia <jenni.jarvinen2@myy.haaga-helia.fi>
2025-03-10 10:06:18 +0200

    Muutettu POST käyttämään lippu- ja myyntiserviceä

--- src/main/java/kevat25/ohjelmistoprojekti1/web/myyntiController.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/web/myyntiController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ import kevat25.ohjelmistoprojekti1.domain.MyyntiRepository;
 import kevat25.ohjelmistoprojekti1.domain.Tapahtuma;
 import kevat25.ohjelmistoprojekti1.domain.Tyontekija;
 import kevat25.ohjelmistoprojekti1.domain.TyontekijaRepository;
+import kevat25.ohjelmistoprojekti1.service.LippuService;
 import kevat25.ohjelmistoprojekti1.service.MyyntiService;
 
 @RestController
@@ -46,31 +48,11 @@ public class myyntiController {
     @Autowired
     private TyontekijaRepository tyontekijaRepository;
 
+    @Autowired
+    private LippuService lippuService; // Injektio LippuService-luokasta
+
     private boolean isValidEmail(String email) {
         return email != null && email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
-    }
-
-    // Lippu-entiteetistä LippuDTO:ksi
-    private LippuDTO lippuToDTO(Lippu lippu) {
-        LippuDTO lippuDTO = new LippuDTO();
-        lippuDTO.setLippuId(lippu.getLippuId());
-        lippuDTO.setTarkistuskoodi(lippu.getTarkistuskoodi());
-
-        // Asetetaan Myynti-id
-        lippuDTO.setMyyntiId(lippu.getMyynti().getMyyntiId()); // Oletetaan, että Lippu on linkitetty Myyntiin
-
-        // Asetetaan tapahtumalippuId
-        lippuDTO.setTapahtumalippuId(lippu.getTapahtumalippu().getTapahtumalippuId());
-
-        // Asetetaan tapahtuman nimi ja alkuajankohta
-        lippuDTO.setTapahtumanNimi(lippu.getTapahtumalippu().getTapahtuma().getTapahtumaNimi());
-        lippuDTO.setAlkuaika(lippu.getTapahtumalippu().getTapahtuma().getAloitusaika());
-
-        // Asetetaan hinta ja asiakastyyppi
-        lippuDTO.setHinta(lippu.getTapahtumalippu().getHinta());
-        lippuDTO.setAsiakastyyppi(lippu.getTapahtumalippu().getAsiakastyyppi().getAsiakastyyppi());
-
-        return lippuDTO;
     }
 
     @Transactional
@@ -79,8 +61,6 @@ public class myyntiController {
 
         // Luodaan uusi Myynti-olio, joka saadaan MyyntiDTO:sta
         Myynti uusiMyynti = new Myynti();
-
-        // Asetetaan MyyntiDTO:n tiedot Myynti-olioon
         uusiMyynti.setMyyntiaika(myyntiDTO.getMyyntiaika());
 
         // Hae työntekijä id:llä
@@ -98,6 +78,7 @@ public class myyntiController {
             Lippu lippu = lippuRepository.findById(lippuDTO.getLippuId())
                     .orElseThrow(() -> new RuntimeException("Lippu ei löytynyt"));
 
+            lippu.setMyynti(uusiMyynti);
             // Liitä lippu oikeaan entiteettiin
             liput.add(lippu);
         }
@@ -108,18 +89,8 @@ public class myyntiController {
         // Tallennetaan Myynti-olio tietokantaan
         Myynti tallennettuMyynti = myyntiRepository.save(uusiMyynti);
 
-        // Muunna Myynti-olio takaisin DTO:ksi ennen palautusta
-        MyyntiDTO tallennettuMyyntiDTO = new MyyntiDTO();
-        tallennettuMyyntiDTO.setMyyntiId(tallennettuMyynti.getMyyntiId());
-        tallennettuMyyntiDTO.setMyyntiaika(tallennettuMyynti.getMyyntiaika());
-        tallennettuMyyntiDTO.setEmail(tallennettuMyynti.getEmail());
-
-        // Muunna liput entiteetistä DTO:ksi
-        List<LippuDTO> lippuDTOList = new ArrayList<>();
-        for (Lippu lippu : tallennettuMyynti.getLiput()) {
-            lippuDTOList.add(lippuToDTO(lippu));
-        }
-        tallennettuMyyntiDTO.setLiput(lippuDTOList);
+        // Muunnetaan tallennettu Myynti DTO:ksi käyttäen MyyntiServiceä
+        MyyntiDTO tallennettuMyyntiDTO = myyntiService.getMyyntiById(tallennettuMyynti.getMyyntiId());
 
         // Palautetaan HTTP 201-vastaus, jossa on MyyntiDTO
         return ResponseEntity.status(HttpStatus.CREATED).body(tallennettuMyyntiDTO);
@@ -184,6 +155,6 @@ public class myyntiController {
         return ResponseEntity.ok(myynti);
     }
 
-    //Muokkaa myyntitapahtuman lippuja (työn alla)
+    // Muokkaa myyntitapahtuman lippuja (työn alla)
 
 }
