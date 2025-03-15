90926b3ceef9412b1a1f635a0e562b2fcf4ddcd9
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-03-15 13:29:30 +0200

    PatchMapping -metodin logiikka siirretty MyyntiServiceen

--- src/main/java/kevat25/ohjelmistoprojekti1/service/MyyntiService.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/service/MyyntiService.java
@@ -4,17 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.persistence.EntityNotFoundException;
-import kevat25.ohjelmistoprojekti1.domain.Lippu;
-import kevat25.ohjelmistoprojekti1.domain.LippuDTO;
-import kevat25.ohjelmistoprojekti1.domain.LippuRepository;
-import kevat25.ohjelmistoprojekti1.domain.Myynti;
-import kevat25.ohjelmistoprojekti1.domain.MyyntiDTO;
-import kevat25.ohjelmistoprojekti1.domain.MyyntiRepository;
-import kevat25.ohjelmistoprojekti1.domain.Tyontekija;
+import kevat25.ohjelmistoprojekti1.domain.*;
 
 @Service
 public class MyyntiService {
@@ -27,6 +24,14 @@ public class MyyntiService {
 
     @Autowired
     private LippuService lippuService; // Service, joka täyttää LippuDTO -objektin datalla
+
+    @Autowired
+    private TyontekijaRepository tyontekijaRepository;
+
+    //Tarkistaa, onko syötetty sähköposti kelvollinen
+    private boolean isValidEmail(String email) {
+        return email != null && email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    }
 
     // Etsii myyntitapahtuman id:n perusteella
     public MyyntiDTO getMyyntiById(Long myyntiId) {
@@ -71,5 +76,29 @@ public class MyyntiService {
             MyyntiDTO myyntiDTO = getMyyntiById(myynti.getMyyntiId()); //Muokkaa myynti entityt MyyntiDTO -objekteiksi
             return myyntiDTO; 
         }).collect(Collectors.toList()); //Kerää myyntiDTO -objektit listaan
+    }
+
+    //Muokkaa myyntitapahtumaa
+    public Myynti muokkaaMyyntia(Long myyntiId, MyyntiDTO myyntiDTO) {
+        Optional<Myynti> myyntiOpt = myyntiRepository.findById(myyntiId);
+        if (myyntiOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        Myynti myynti = myyntiOpt.get();
+
+        if (!isValidEmail(myyntiDTO.getEmail())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        myynti.setEmail(myyntiDTO.getEmail());
+
+        if (myyntiDTO.getTyontekijaId() != null) {
+            Optional<Tyontekija> tyontekijaOpt = tyontekijaRepository.findById(myyntiDTO.getTyontekijaId());
+            if (tyontekijaOpt.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+            myynti.setTyontekija(tyontekijaOpt.get());
+        }
+
+        return myyntiRepository.save(myynti);
     }
 }
