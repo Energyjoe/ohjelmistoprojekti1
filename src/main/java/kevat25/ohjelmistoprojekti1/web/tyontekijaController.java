27a61ef134b82f4896dad3a447b8fd078f7e39d3
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-03-23 21:31:31 +0200

    DeleteMapping lisätty työntekijä controlleriin

--- src/main/java/kevat25/ohjelmistoprojekti1/web/tyontekijaController.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/web/tyontekijaController.java
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
-import kevat25.ohjelmistoprojekti1.domain.SalasanaUpdateDTO;
-import kevat25.ohjelmistoprojekti1.domain.TyontekijaCreateDTO;
-import kevat25.ohjelmistoprojekti1.domain.TyontekijaResponseDTO;
-import kevat25.ohjelmistoprojekti1.domain.TyontekijaUpdateDTO;
+import kevat25.ohjelmistoprojekti1.domain.*;
 import kevat25.ohjelmistoprojekti1.service.TyontekijaService;
+
+import java.util.Optional;
 
 
 
@@ -24,8 +26,14 @@ import kevat25.ohjelmistoprojekti1.service.TyontekijaService;
 @RequestMapping("/tyontekijat")
 public class tyontekijaController {
 
+    private final TyontekijaRepository tyontekijaRepository;
+
     @Autowired
     private TyontekijaService tyontekijaService;
+
+    tyontekijaController(TyontekijaRepository tyontekijaRepository) {
+        this.tyontekijaRepository = tyontekijaRepository;
+    }
 
     //Lisää työntekijä
     @PostMapping("/")
@@ -51,6 +59,20 @@ public class tyontekijaController {
 
         tyontekijaService.updateSalasana(tyontekijaId, salasanaDto);
         return ResponseEntity.ok("Salasana vaihdettu onnistuneesti");
+    }
+
+    //Poista työntekijä
+    @DeleteMapping("/{tyontekijaId}")
+    @ResponseBody
+    public ResponseEntity<String> deleteTyontekija(@PathVariable Long tyontekijaId) {
+
+        Optional<Tyontekija> tyontekija = tyontekijaRepository.findById(tyontekijaId);
+        if(tyontekija.isPresent()) {
+            tyontekijaRepository.delete(tyontekija.get());
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Työntekijä poistettu onnistuneesti");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Työntekijää ei löytynyt");
+        }
     }
 
 }
