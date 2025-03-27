92f72fde384c822cc7adcd35cf7566d232e124a6
Lehtimaeki <anni.tuulevi.lehtimaki@gmail.com>
2025-03-27 20:51:08 +0200

    Salasanan tarkistusmetodi luotu

--- src/main/java/kevat25/ohjelmistoprojekti1/service/SalasanaTarkistusService.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/service/SalasanaTarkistusService.java
@@ -1,5 +1,19 @@
 package kevat25.ohjelmistoprojekti1.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SalasanaTarkistusService {
 
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    
+    public SalasanaTarkistusService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public boolean tarkistaSalasana(String rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
 }
