f42f89489d745f47caad677d9b0eed8a6b5d1398
Lehtimaeki <anni.tuulevi.lehtimaki@gmail.com>
2025-04-09 19:38:31 +0300

    Lisätty NonNull-annotaatiota JWT-serviceen

--- src/main/java/kevat25/ohjelmistoprojekti1/service/JwtFilter.java
+++ src/main/java/kevat25/ohjelmistoprojekti1/service/JwtFilter.java
@@ -8,11 +8,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.lang.NonNull;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 
 // https://www.youtube.com/watch?v=ZhtF4i-iB1A #38 Spring Security | Validating JWT Token
 
@@ -23,7 +25,9 @@ public class JwtFilter extends OncePerRequestFilter {
     private JwtService jwtService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, 
+                                    @NonNull HttpServletResponse response, 
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         // Hae Authorization-otsake pyynnöstä
